e27ca1221f2063bff1911750e71f49eaee128a5c
TARUN KUMAR <130458471+tarunmariyo@users.noreply.github.com>
2025-04-14 21:40:08 +0530

    Update section title from "My Portfolio" to "My Projects" for clarity

--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -88,7 +88,7 @@ const Portfolio = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
       >
-        My Portfolio
+        My Projects
       </SectionTitle>
       <ProjectsGrid>
         {projects.map((project, index) => (
